fe852f7c511e9a1562af8a4a1ecad01f3e316c1a
bogdan9999 <prodamwsefree@gmail.com>
2018-04-01 19:16:07 +0800

    init commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="endline"></div>
+<!-- footer start -->
+	<div id="footer" class="clearfix">
+		<div class="credit">
+				Copyright &copy; <?php echo date("Y"); ?>
+				<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>.
+				All rights reserved. Powered by WordPress.<br/>
+				<div class="footer_c"><a class="copyr" href="http://wordpress-theming.ru" >wordpress-theming</a> & <a href="http://wp-templates.ru/">Темы WordPress</a> & <a href="http://hostband.ru/">хостинг</a></div>
+		</div>
+	</div>
+<!-- footer end -->
+</div></div></div></div>
+<!-- wrapper end -->
+<?php wp_footer(); ?>
+</body>
+</html>
